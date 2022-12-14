218a4e28da776d479ac615d4b763ee3483c25dcb
Daniel Young <Danyoung91@gmail.com>
2022-12-14 16:11:47 -0500

    commit tree 83d52af79f8c3278967b047a06698be2baebaf01

--- index.js
+++ index.js
@@ -25,26 +25,111 @@ function myMap(collection, callback) {
   return newArray;
 }
 
-function myReduce(collection, callback, acc = 0) {
+function myFind(collection, predicate) {
   if (Array.isArray(collection)) {
     for (let item of collection) {
-      acc = callback(acc, item);
+      if (predicate(item) == true) {
+        return item;
+      }
     }
   } else {
     for (let item in collection) {
-      acc = callback(acc, collection[item]);
+      if (predicate(item) == true) {
+        return item;
+      }
     }
   }
+}
 
+function myFilter(collection, predicate) {
+  let newArray = [];
+  if (Array.isArray(collection)) {
+    for (let item of collection) {
+      if (predicate(item) == true) {
+        newArray.push(item);
+      }
+    }
+  } else {
+    for (let item in collection) {
+      if (predicate(item) == true) {
+        newArray.push(item);
+      }
+    }
+  }
+  return newArray;
+}
+
+function mySize(collection) {
+  let acc = 0;
+  if (Array.isArray(collection)) {
+    for (let item of collection) {
+      acc++;
+    }
+  } else {
+    for (let item in collection) {
+      acc++;
+    }
+  }
   return acc;
 }
 
-console.log(
-  myReduce(
-    [1, 2, 3],
-    function (acc, val) {
-      acc + val;
-    },
-    10
-  )
-);
+function myFirst(array, n) {
+  let arr = [];
+  if (n == undefined) {
+    return array[0];
+  } else {
+    for (let i = 0; i < n; i++) {
+      arr.push(array[i]);
+    }
+  }
+  return arr;
+}
+
+function myLast(array, n) {
+  if (n == undefined) {
+    return array[array.length - 1];
+  } else {
+    return array.slice(array.length - n);
+  }
+}
+
+function myKeys(obj) {
+  let arr = [];
+  for (let item in obj) {
+    arr.push(item);
+  }
+  return arr;
+}
+
+function myValues(obj) {
+  let arr = [];
+  for (let item in obj) {
+    arr.push(obj[item]);
+  }
+  return arr;
+}
+let input = function (collection) {
+  return collection instanceof Array
+    ? collection.slice()
+    : Object.values(collection);
+};
+
+function myReduce(collection, callback, acc) {
+  let newCollection = input(collection);
+
+  if (!acc) {
+    acc = newCollection[0];
+    newCollection = newCollection.slice(1);
+  }
+
+  for (let item of newCollection) {
+    acc = callback(acc, item, newCollection);
+  }
+  return acc;
+}
+
+const callback = (acc, val, collection) => {
+  return acc + val * 3;
+};
+
+console.log(myReduce([1, 2, 3, 4], callback));
